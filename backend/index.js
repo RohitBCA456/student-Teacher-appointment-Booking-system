a0ec14aec9373a86dc0fdf0944d889c9aac3ad1a
RohitBCA456 <ry2281104@gmail.com>
2025-07-25 12:43:54 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
 import { app, server } from "./app.js";
 import { connectDB } from "./database/db.js";
 
-connectDB().then(() => {
-  app.on("Error", (error) => {
-    console.error("Server encountered an error:", error);
-  });
-  app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-  });
-});
+const PORT = process.env.PORT || 3000; // use PORT for both HTTP and Socket
 
-server.listen(process.env.SOCKET_PORT, () => {
-  console.log(`Socket server is running on port ${process.env.SOCKET_PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.error(`Server error: ${err.message}`);
+    });
+
+    server.listen(PORT, () => {
+      console.log(`Server & Socket.IO running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Failed to connect to the database: ${error.message}`);
+    process.exit(1);
+  });
